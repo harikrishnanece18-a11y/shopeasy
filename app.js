cff57d1f8c6d3affdcf63bd52518ef5b4029d3c5
Hari Krishnan <harikrishnanece18@gmail.com>
2025-11-03 17:39:38 +0530

    App.js :changes in messages

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const name='CodeTest';
 
 app.get("/",(req,res)=>
 {
-    const message='welcome to shopEasy from harikrishnan!'
+    const message='welcome to shopEasy from code test'
     res.send(message);
 
 })
