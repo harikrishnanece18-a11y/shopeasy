4b69f58bec8e405ae24897ba06cf0294feb006b6
Hari Krishnan <harikrishnanece18@gmail.com>
2025-11-03 16:06:13 +0530

    initial commit:Basic Express app setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app=express();
+const PORT=process.env.PORT || 3000;
+const name='CodeTest';
+
+app.get("/",(req,res)=>
+{
+    const message='welcome to shopEasy from ${name}!'
+    res.send(message);
+
+})
+
+const server =app.listen(PORT,()=>{
+    console.log('server is running on port ${PORT}');
+})
+module.exports ={app,server,name} 
